ebed637d92a764208c55d1de1f4f9b3f59969bab
Scott Spicer <scottspicer24@gmail.com>
2024-05-02 19:29:35 -0400

    added react-navigation and a screens folder, directed main app to screens using navigation tags

---
+++ app/screens/List.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function List() {
+  return (
+    <View>
+      <Text>List 1</Text>
+    </View>
+  )
+}
+
+//export default List
